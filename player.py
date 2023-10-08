fb422da25777d1f031e0ee337058fad30eb46888
Asif Ahmed <iamasif93@gmail.com>
2023-10-08 20:34:17 +0600

    turtle-crossing-game v1.0

---
+++ player.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.goto_start()
+        self.seth(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
+    def goto_start(self):
+        self.goto(STARTING_POSITION)
+
+    def at_finish(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
